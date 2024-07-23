1bc9e991e2c02f9ece9b2d63f007f6df170d6a8c
Ishu Mehta <mehta.ishu14@gmail.com>
2024-07-23 20:54:17 +0530

    added NotFoundPage css

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./NotFoundPage.module.css";
 
-function NotFoundPage() {
+function NotFound() {
 	return (
 		<div className={styles.container}>
-			<h1>404 Not Found</h1>
-			<p>Sorry, the page you are looking for does not exist.</p>
+			<h1 className={styles.heading}>404</h1>
+			<p className={styles.text}>Oops! Page not found.</p>
+			<p className={styles.subText}>
+				The page you are looking for might have been removed, had its name
+				changed, or is temporarily unavailable.
+			</p>
+			<Link to="/" className={styles.link}>
+				Go to Homepage
+			</Link>
 		</div>
 	);
 }
 
-export default NotFoundPage;
+export default NotFound;
