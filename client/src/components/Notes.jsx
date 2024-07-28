5ca5105dacff7ab57ae37c7200a24f81ef862940
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-07-28 15:32:05 +0530

    added notes
    notes with working functionality

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./Notes.module.css";
 
 function Notes() {
+  const[input,setInput]=useState(
+    localStorage.getItem("hi") || null
+  )
+  const changehandler =(e)=>{
+    setInput(e.target.value)
+    localStorage.setItem("hi",input)
+  }
+  useEffect(()=>{
+    changehandler
+  },[input])
   return (
-    <div className={'styles.container'}>        
-      hiasf
+    <div className={styles.container}>        
+      <div className={styles.heading}>
+        All Notes
+      </div>
+      <div className={styles.body}>
+        <textarea value={input} onChange={changehandler}></textarea>
+      </div>
     </div>
   )
 }
