9bbd45fd462f6ef49bdfe12a9f6b474e26fe9b52
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-07-28 13:50:22 +0530

    Added the Noteswidget

---
+++ client/src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Notes.module.css";
+
+function Notes() {
+  return (
+    <div className={'styles.container'}>        
+      hiasf
+    </div>
+  )
+}
+
+export default Notes
--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import styles from "./DashboardPage.module.css";
 import TimerWidget from "../components/TimerWidget";
-
+import WeatherWidget from "../components/WeatherWidget";
+import ProfileWidget from "../components/ProfileWidget";
+import NewsWidget from "../components/NewsWidget";
+import Notes from "../components/Notes";
 function DashboardPage() {
 	return (
 		<div className={styles.container}>
-			<div className={styles.div1}></div>
-			<div className={styles.div2}></div>
+			<div className={styles.div1}>
+				<ProfileWidget/>
+			</div>
+			<div className={styles.div2}>
+				<WeatherWidget/>
+			</div>
 			<div className={styles.div3}>
 				<TimerWidget />
 			</div>
-			<div className={styles.div4}></div>
-			<div className={styles.div5}></div>
+			<div className={styles.div4}>
+				<Notes/>
+			</div>
+			<div className={styles.div5}>
+				<NewsWidget/>
+			</div>
 		</div>
 	);
 }
