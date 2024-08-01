c34ea4db0804766820e18cc3effebe0bc29d77f8
Ishu Mehta <mehta.ishu14@gmail.com>
2024-08-01 21:40:43 +0530

    used container query in Profile Widget

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AppContext } from "./context/AppContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import GenrePage from "./pages/GenrePage";
@@ -7,17 +9,58 @@ import NotFoundPage from "./pages/NotFoundPage";
 import DashboardPage from "./pages/DashboardPage";
 import MoviePage from "./pages/MoviePage";
 
+const ProtectedRoute = ({ children }) => {
+	const { user, selectedGenres } = useContext(AppContext);
+
+	return user ? children : <Navigate to="/register" />;
+};
+
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<RegisterPage />} />
 				<Route path="/register" element={<RegisterPage />} />
-				<Route path="/home" element={<RegisterPage />} />
-				<Route path="/genres" element={<GenrePage />} />
-				<Route path="/carousel" element={<CarouselPage />} />
-				<Route path="/dashboard" element={<DashboardPage />} />
-				<Route path="/movies" element={<MoviePage />} />
+				<Route
+					path="/home"
+					element={
+						<ProtectedRoute>
+							<GenrePage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/genres"
+					element={
+						<ProtectedRoute>
+							<GenrePage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/carousel"
+					element={
+						<ProtectedRoute>
+							<CarouselPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/dashboard"
+					element={
+						<ProtectedRoute>
+							<DashboardPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/movies"
+					element={
+						<ProtectedRoute>
+							<MoviePage />
+						</ProtectedRoute>
+					}
+				/>
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</BrowserRouter>
